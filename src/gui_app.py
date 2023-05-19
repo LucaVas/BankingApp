cec7139bd058cbe7930643c62d9b6b2dfd08a413
Luca Vassos <luca.vassos@gmail.com>
2023-05-19 17:57:31 +0300

    Created 3 entry interfaces & fields checking

--- src/gui_app.py
+++ src/gui_app.py
@@ -4,17 +4,29 @@ ctk.set_default_color_theme("green")  # Themes: "blue" (standard), "green", "dar
 ctk.set_appearance_mode("dark") # "system" (default), "dark", "light"
 
 
-# Presentation window
-        
-class WelcomeWindow(ctk.CTk):
+class App(ctk.CTk):
+    """
+    Main class where I run my application
+    """
+
     def __init__(self):
         super().__init__()
 
         self.title("Luca's bank")
         self.width = "500"
-        self.height = "400"
-
+        self.height = "300"
         self.geometry(f"{self.width}x{self.height}")
+
+
+
+        
+class WelcomeWindow(App):
+    """
+    Class which contains my first welcome window
+    """
+    def __init__(self):
+        super().__init__()
+
         self.grid_columnconfigure((0,1), weight=2)
         self.grid_rowconfigure((0,1), weight=1)
 
@@ -32,67 +44,137 @@ class WelcomeWindow(ctk.CTk):
 
         # Start button
         self.start_button = ctk.CTkButton(self, width=40, height=40, text="Start now", text_color="black", font=("tahoma", 16), command=self.button_callback)
-        self.start_button.grid(row=1, column=1, padx=20, pady=20, sticky="ew", columnspan=2)
+        self.start_button.grid(row=1, column=1, padx=20, pady=20, sticky="ew")
 
     def button_callback(self):
-        print("button clicked")
+        holder_registration_window = HolderRegistrationWindow()
+        self.destroy()
+        holder_registration_window.mainloop()
+        
 
         
 
 
-    
+class HolderRegistrationWindow(App):
+    """
+    Class which contains the window of holder registration
+    """
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.grid_columnconfigure((0,1), weight=1)
+        self.grid_rowconfigure((0,1,2,3,4,5), weight=1)
+
+        self.content: dict[str, str] = {
+            "first": "",
+            "last": "",
+            "birth_date": ""            
+        }
+
+
+        # Title: Registration
+        self.welcome_label = ctk.CTkLabel(self, text="Registration", fg_color="transparent", text_color="white", font=("tahoma", 24))
+        self.welcome_label.grid(row=0, column=0, padx=10, pady=(10,10), sticky="w")
+
+        # Registration entries
+
+        self.first_name_entry = ctk.CTkEntry(self, width=300, height=20, text_color="white", font=("tahoma", 15), placeholder_text="Your name")
+        self.first_name_entry.grid(row=1, column=0, columnspan=2, padx=10, pady=(10,10), sticky="ew")
+
+        self.last_name_entry = ctk.CTkEntry(self, width=300, height=20, text_color="white", font=("tahoma", 15), placeholder_text="Your surname")
+        self.last_name_entry.grid(row=2, column=0, columnspan=2, padx=10, pady=(10,10), sticky="ew")
+
+        self.birth_date_entry = ctk.CTkEntry(self, width=300, height=20, text_color="white", font=("tahoma", 15), placeholder_text="Your date of birth")
+        self.birth_date_entry.grid(row=3, column=0, columnspan=2, padx=10, pady=(10,10), sticky="ew")
+
+        # Error log lable
+        self.error_label = ctk.CTkLabel(self, text="", fg_color="transparent", text_color="red", font=("tahoma", 15))
+        self.error_label.grid(row=4, column=0, padx=10, pady=(10,10), sticky="w")
+
+        # Continue button
+
+        self.start_button = ctk.CTkButton(self, width=40, height=40, text="Continue", text_color="black", font=("tahoma", 16), command=self.save_content)
+        self.start_button.grid(row=5, column=1, padx=20, pady=20, sticky="ew")
+
+
+    def save_content(self) -> None:
+
+        self.content["first"] = self.first_name_entry.get()
+        self.content["last"] = self.last_name_entry.get()
+        self.content["birth_date"] = self.birth_date_entry.get()
+
+        self.check_content()
+
+    def check_content(self) -> None:
+
+        if not self.content["first"] or not self.content["last"] or not self.content["birth_date"]:
+            self.error_label.configure(text="Some fields are empty!")
+        else:
+            self.next_window()
         
-    
+
+    def next_window(self) -> None:
+
+        password_registration_window = PasswordRegistrationWindow()
+        self.destroy()
+        password_registration_window.mainloop()   
         
-class App(ctk.CTk):
+
+
+
+class PasswordRegistrationWindow(App):
+    """
+    Class which contains the window of holder registration
+    """
     def __init__(self):
         super().__init__()
 
-        self.title("Luca's bank")
-        self.width = "400"
-        self.height = "500"
+        self.grid_columnconfigure((0,1), weight=1)
+        self.grid_rowconfigure((0,1,2,3,4), weight=1)
+
+        self.content: dict[str, str] = {
+            "password": "",
+            "repeated_password": "",
+        }
+
+        # Title: Registration
+        self.welcome_label = ctk.CTkLabel(self, text="Registration", fg_color="transparent", text_color="white", font=("tahoma", 24))
+        self.welcome_label.grid(row=0, column=0, padx=10, pady=(10,10), sticky="w")
+
+        # Registration entries
+
+        self.password_entry = ctk.CTkEntry(self, width=300, height=20, text_color="white", font=("tahoma", 15), placeholder_text="Enter your password")
+        self.password_entry.grid(row=1, column=0, columnspan=2, padx=10, pady=(10,10), sticky="ew")
+
+        self.repeated_password_entry = ctk.CTkEntry(self, width=300, height=20, text_color="white", font=("tahoma", 15), placeholder_text="Repeat your password")
+        self.repeated_password_entry.grid(row=2, column=0, columnspan=2, padx=10, pady=(10,10), sticky="ew")
+
+        # Label: Disclaimer
+        self.welcome_label = ctk.CTkLabel(self, text="Attention: if you loose your password, you will not be able to access.", fg_color="transparent", text_color="white", font=("tahoma", 12))
+        self.welcome_label.grid(row=3, column=0, columnspan=2, padx=10, pady=(10,10), sticky="w")
+
+        # Continue button
+
+        self.start_button = ctk.CTkButton(self, width=50, height=40, text="Confirm registration", text_color="black", font=("tahoma", 16), command=self.save_content)
+        self.start_button.grid(row=4, column=1, padx=20, pady=20, sticky="ew")
 
 
-        self.geometry(f"{self.width}x{self.height}")
-        self.grid_columnconfigure((0), weight=1)
-        self.grid_rowconfigure((3), weight=1)
+    def save_content(self) -> None:
+        self.content["password"] = self.password_entry.get()
+        self.content["repeated_password"] = self.repeated_password_entry.get()
+
+        self.check_password()
 
 
-        # self.button = ctk.CTkButton(self, text="my button", command=self.button_callback)
-        # self.button.grid(row=0, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
+    def check_password(self) -> None:
+        if not self.content["password"] or not self.content["repeated_password"] or self.content["password"] != self.content["repeated_password"]:
+            self.welcome_label.configure(text="The two passwords don't match!", text_color="red")
+        elif self.content["password"] == self.content["repeated_password"]:
+            self.button_callback()
+        else:
+            raise ValueError
 
-        # self.checkbox_frame = CheckboxFrame(self)
-        # self.checkbox_frame.grid(row=1, column=0, padx=10, pady=(10,10), sticky="nsw")
-
-        # self.entry_frame = HolderInformationFrame(self)
-        # self.entry_frame.grid(row=2, column=0, padx=10, pady=(10,10))
-
-
-
-class HolderInformationFrame(ctk.CTkFrame):
-    def __init__(self, master):
-        super().__init__(master)
-
-        self.entry = ctk.CTkEntry(self, width=300, height=10, text_color="white", font=("tahoma", 20), placeholder_text="Name")
-        self.entry.grid(row=0, column=0, padx=10, pady=(10,10), sticky="w")
+    def button_callback(self):
+        self.destroy()
         
-        self.entry = ctk.CTkEntry(self, width=300, height=10, text_color="white", font=("tahoma", 20), placeholder_text="Surname")
-        self.entry.grid(row=1, column=0, padx=10, pady=(10,10), sticky="w")
 
-        self.entry = ctk.CTkEntry(self, width=300, height=10, text_color="white", font=("tahoma", 20), placeholder_text="Date of birth")
-        self.entry.grid(row=2, column=0, padx=10, pady=(10,10), sticky="w")
-        
-
-
-
-
-class CheckboxFrame(ctk.CTkFrame):
-    def __init__(self, master):
-        super().__init__(master)
-
-        self.checkbox_1 = ctk.CTkCheckBox(self, text="checkbox 1")
-        self.checkbox_1.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
-        self.checkbox_2 = ctk.CTkCheckBox(self, text="checkbox 2")
-        self.checkbox_2.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
-        self.checkbox_3 = ctk.CTkCheckBox(self, text="checkbox 3")
-        self.checkbox_3.grid(row=2, column=0, padx=10, pady=(10,0), sticky="w")
